c87b1b7f91260d88a0516a1222d3887469adf7b6
Olga <olga609@mail.ru>
2022-10-10 18:21:51 +0300

    Third_Task

--- Home_work_4.py
+++ Home_work_4.py
@@ -34,4 +34,42 @@ def Factor(n):
 print(Factor(int(input())))
 
 
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности в том же порядке.
+# in
+# 7
+
+# out
+# [4, 5, 3, 3, 4, 1, 2]
+# [5, 1, 2]
+
+# in
+# -1
+
+# out
+# Negative value of the number of numbers!
+# []
+
+# in
+# 10
+
+# out
+# [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
+# [6, 2, 3, 0, 9]
+
+numbers = [1, 2, 5, 4, 6, 5, 4, 2, 9]
+
+def get_unique_numbers(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+
+print(get_unique_numbers(numbers))
+
+
+
 
