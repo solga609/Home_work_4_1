9af012de9055433b1da58fa60e6fa3986365f306
Olga <olga609@mail.ru>
2022-10-10 18:18:18 +0300

    Home_Task_2

--- Home_work_4.py
+++ Home_work_4.py
@@ -16,3 +16,22 @@
 num = float(input("Enter a real number: "))
 accu = input("enter the required accurancy '0.0001' : ").split(".")
 print(f"{num:.{len(accu)}f}")
+
+# 2.Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N
+
+def Factor(n):
+    Num = []
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            Num.append(d)
+            n //= d
+        else:
+            d += 1
+    if n > 1:
+        Num.append(n)
+    return Num
+print(Factor(int(input())))
+
+
+
